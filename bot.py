f2f97ee1ee6e5e4199fc46d336d0faa6c82ae1fe
Mohamadreza <161870762+theothermmd@users.noreply.github.com>
2024-12-06 01:07:08 +0330

    dasd

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from io import BytesIO
 from tqdm import tqdm
 from colorama import Fore, Style
 count = 0
+apikeys = ['6273c114' , '8812c608' , '42a575eb' , '54a50e39']
 bearer_token : str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3ZvZC50YXJpbmV0LmlyIiwiaWF0IjoxNzMzMTY0ODg2LCJuYmYiOjE3MzMxNjQ4ODYsImV4cCI6MTczMzc2OTY4NiwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMyJ9fX0.c1NIRwSq2rmFeIZ-OBS7hu32TuD1GwSqwUVPPCVB3To"
 cdn : dict = {
     "poster": "https://s35.upera.net/thumb?w=675&h=1000&q=90&src=https://s35.upera.net/s3/posters/",
@@ -149,12 +150,11 @@ with tqdm(total=total_items, bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{per
                             genres.append(ls['id'])
 
                 country = []
-                apikeys = ['6273c114' , '8812c608' , '42a575eb' , '54a50e39']
-                apikey = apikeys[count]
+                
                 if movie_data['isirani'] != 1 and movie_data['imdb'] != None and movie_data['imdb'] != "" :
                     for attempt in range(3):
                         try:
-                            imdb_data = requests.get( f"http://www.omdbapi.com/?i={movie_data['imdb']}&apikey={apikey}", headers=headers, timeout=30).json()
+                            imdb_data = requests.get( f"http://www.omdbapi.com/?i={movie_data['imdb']}&apikey={apikeys[count]}", headers=headers, timeout=30).json()
                             break
 
                         except:
